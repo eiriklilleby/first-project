8b1b83d9d2fe90ed11269b82cbd8d724dc087564
eiriklilleby <71594873+eiriklilleby@users.noreply.github.com>
2021-04-18 21:42:26 +0200

    added products from api

---
+++ js/details.js
@@ -0,0 +1,44 @@
+const detailContainer = document.querySelector(".container");
+
+const queryString = document.location.search;
+const params = new URLSearchParams(queryString);
+const id = params.get("id");
+
+async function makeApiCall() {
+  try {
+    const response = await fetch("http://flowerpowerstore.eu/wp-json/wc/store/products/" + id);
+
+    const json = await response.json();
+    console.log(json);
+    detailContainer.innerHTML = "";
+
+    detailContainer.innerHTML = `
+    <div class="game-img">
+    <img src="${json.images[0].src}" alt="game cover of ${json.name}" />
+    </div>
+    <div class="game-info">
+    <h1>${json.name}</h1>
+    <span class="prev-price">>${json.prices.regular_price}£</span>
+    <span class="current-price">${json.prices.price}£</span>
+    <p class="description">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem dolor eos nisi non, dolorum rem omnis
+    numquam voluptatibus repudiandae sequi cum magnam. Assumenda eaque magni obcaecati cupiditate, nisi est id
+    non dicta corporis ab accusantium reiciendis doloribus odio molestiae quos neque quae ratione quam provident
+    veniam excepturi deleniti animi nostrum.
+    </p>
+    <a class="gamebutton" href="checkout.html">Add to cart</a>
+    </div>
+    <div class="game-images">
+    <img src="${json.images[1].src}" alt="game image of${json.name}" />
+    <img src="${json.images[2].src}" alt="game image of${json.name}" />
+    </div>
+       `;
+
+    document.title = json.name;
+  } catch (error) {
+    detailContainer.innerHTML = displayError();
+    console.log(error);
+  }
+}
+
+makeApiCall();
---
+++ js/script.js
@@ -0,0 +1,88 @@
+const url = "http://flowerpowerstore.eu/wp-json/wc/store/products";
+const specificUrl = "http://flowerpowerstore.eu/wp-json/wc/store/products/146";
+const gameImage = document.querySelector(".games-container");
+const mainImage = document.querySelector(".highlightedimg");
+
+async function makeApiCall() {
+  try {
+    const response = await fetch(url);
+    const json = await response.json();
+
+    gameImage.innerHTML = "";
+
+    console.log(json);
+    json.forEach((games) => {
+      gameImage.innerHTML += `
+      <div class="container-games">
+      <div class="game">
+      <a href="details.html?id=${games.id}">
+      <img src="${games.images[0].src}" alt="game cover of ${games.name}"></img>
+      </a>
+      </div>
+      <div class="box-info">
+     
+      <div class="spanbox">
+      <span class="prev-featured">${games.prices.regular_price}£</span>
+      <span class="current-featured">${games.prices.price}£</span>
+      </div>
+      <img id="steam" src="/images/icons/steamlogo.png" alt="Steam logo" />
+      </div>
+      <div class="buttonbox">
+      <a href="checkout.html">
+      <button class="boxgame1">
+      <img id="shopping" src="images/icons/shopping-cart.png" alt="Shopping cart" />ADD TO CART
+      </button></a>
+      </div>
+      </div>
+                                                                `;
+    });
+  } catch (error) {
+    gameImage.innerHTML = displayError();
+    console.log("An error has occured");
+  }
+}
+
+makeApiCall();
+
+async function singleApiCall() {
+  try {
+    const response = await fetch(specificUrl);
+    const json = await response.json();
+    console.log(json.images[0].src);
+
+    mainImage.innerHTML = "";
+
+    mainImage.style.backgroundImage = `url(${json.images[0].src})`;
+    mainImage.innerHTML = `<div class="highlighted-overlay">
+          <div class="highlighted-game-text">
+            <h2>
+              <a href="details.html?id=${json.id}">${json.name}</a>
+            </h2>
+            <p>Standard Edtion</p>
+            <p>PC</p>
+          </div>
+          <div class="price-container">
+            <div class="discount">
+              <p>-11%</p>
+            </div>
+            <p class="prices-info">
+              <span class="prev-featured">${json.prices.regular_price}£</span>
+      <span class="current-featured">${json.prices.price}£</span>
+            </p>
+          </div>
+          <div class="container-btn">
+            <a href="checkout.html">
+              <button class="pre-purchase">
+                <img id="shopping" src="/images/icons/shopping-cart.png" alt="Shopping cart" />
+                PRE-PURCHASE
+              </button></a
+            >
+          </div>
+        </div>`;
+  } catch (error) {
+    mainImage.innerHTML = displayError();
+    console.log(error);
+  }
+}
+
+singleApiCall();
